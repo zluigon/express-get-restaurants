244a815360acf86d3c039da7bd9b99c0f6f38e66
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-23 15:47:07 -0400

    Part 3 - POST, PUT, DELETE complete

--- src/app.js
+++ src/app.js
@@ -4,13 +4,40 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below:
+// get all
 app.get("/restaurants", async (req, res) => {
-  let allRestaurants = await Restaurant.findAll();
+  const allRestaurants = await Restaurant.findAll();
   res.json(allRestaurants);
 });
 
+// get by ID
 app.get("/restaurants/:id", async (req, res) => {
-  let foundRestaurant = await Restaurant.findByPk(req.params.id);
+  const foundRestaurant = await Restaurant.findByPk(req.params.id);
   res.json(foundRestaurant);
 });
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+// create
+app.post("/restaurants", async (req, res) => {
+  const newRestaurant = await Restaurant.create(req.body);
+  res.json(newRestaurant);
+});
+
+// update
+app.put("/restaurants/:id", async (req, res) => {
+  const currRestaurant = await Restaurant.findByPk(req.params.id);
+  const updatedRestaurant = await currRestaurant.update(req.body);
+  res.json(updatedRestaurant);
+});
+
+// delete
+app.delete("/restaurants/:id", async (req, res) => {
+  const currRestaurant = await Restaurant.findByPk(req.params.id);
+  const deletedRestaurant = await currRestaurant.destroy();
+  res.json(deletedRestaurant);
+});
+
 module.exports = app;
