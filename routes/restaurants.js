f1c906edb9d6a12cc84d15f30f7f69f51e2023b1
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-24 15:18:27 -0400

    Update: Part 4 - Express Router complete

---
+++ routes/restaurants.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const Restaurant = require("../models/.");
+
+// get
+router.get("/", async (req, res, next) => {
+  try {
+    const allRestaurants = await Restaurant.findAll();
+    res.json(allRestaurants);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// get by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const foundRestaurant = await Restaurant.findByPk(req.params.id);
+    res.json(foundRestaurant);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// create
+router.post("/", async (req, res, next) => {
+  try {
+    const newRestaurant = await Restaurant.create(req.body);
+    res.json(newRestaurant);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// update
+router.put("/:id", async (req, res, next) => {
+  try {
+    const currRestaurant = await Restaurant.findByPk(req.params.id);
+    const updatedRestaurant = await currRestaurant.update(req.body);
+    res.json(updatedRestaurant);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// delete
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const currRestaurant = await Restaurant.findByPk(req.params.id);
+    const deletedRestaurant = await currRestaurant.destroy();
+    res.json(deletedRestaurant);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -2,42 +2,46 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
-
-//TODO: Create your GET Request Route Below:
-// get all
-app.get("/restaurants", async (req, res) => {
-  const allRestaurants = await Restaurant.findAll();
-  res.json(allRestaurants);
-});
-
-// get by ID
-app.get("/restaurants/:id", async (req, res) => {
-  const foundRestaurant = await Restaurant.findByPk(req.params.id);
-  res.json(foundRestaurant);
-});
+const restaurantRoutes = require("../routes/restaurants");
 
 app.use(express.json());
-
 app.use(express.urlencoded());
 
-// create
-app.post("/restaurants", async (req, res) => {
-  const newRestaurant = await Restaurant.create(req.body);
-  res.json(newRestaurant);
-});
+app.use("/restaurants", restaurantRoutes);
 
-// update
-app.put("/restaurants/:id", async (req, res) => {
-  const currRestaurant = await Restaurant.findByPk(req.params.id);
-  const updatedRestaurant = await currRestaurant.update(req.body);
-  res.json(updatedRestaurant);
-});
+// routes moved to dedicated route handler
 
-// delete
-app.delete("/restaurants/:id", async (req, res) => {
-  const currRestaurant = await Restaurant.findByPk(req.params.id);
-  const deletedRestaurant = await currRestaurant.destroy();
-  res.json(deletedRestaurant);
-});
+// //TODO: Create your GET Request Route Below:
+// // get all
+// app.get("/restaurants", async (req, res) => {
+//   const allRestaurants = await Restaurant.findAll();
+//   res.json(allRestaurants);
+// });
+
+// // get by ID
+// app.get("/restaurants/:id", async (req, res) => {
+//   const foundRestaurant = await Restaurant.findByPk(req.params.id);
+//   res.json(foundRestaurant);
+// });
+
+// // create
+// app.post("/restaurants", async (req, res) => {
+//   const newRestaurant = await Restaurant.create(req.body);
+//   res.json(newRestaurant);
+// });
+
+// // update
+// app.put("/restaurants/:id", async (req, res) => {
+//   const currRestaurant = await Restaurant.findByPk(req.params.id);
+//   const updatedRestaurant = await currRestaurant.update(req.body);
+//   res.json(updatedRestaurant);
+// });
+
+// // delete
+// app.delete("/restaurants/:id", async (req, res) => {
+//   const currRestaurant = await Restaurant.findByPk(req.params.id);
+//   const deletedRestaurant = await currRestaurant.destroy();
+//   res.json(deletedRestaurant);
+// });
 
 module.exports = app;
